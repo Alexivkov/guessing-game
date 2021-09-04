c0b37d0e9fb2be265a908f79e8b8badaebe8e42e
alexivkov <sivkov@inbox.ru>
2021-09-04 21:11:59 +0300

    feat: add three functions

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,24 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+         this.guess = function(){
+              return Math.ceil(this.min / 2 + this.max / 2);
+         
+         }
+         return this.guess;
     }
 
     lower() {
-
+        this.max = this.guess();
     }
 
     greater() {
-
+            this.min = this.guess();
     }
 }
 
